d38d438a9cac07541d65bc9827247e7f4fe07a47
Andrey <Andrey@electis.ru>
2023-07-05 18:01:13 +0700

    playlist

--- dl_youtube.py
+++ dl_youtube.py
@@ -6,23 +6,38 @@ import settings
 import utils
 
 
-def download():
-    root_path = Path(settings.root)
-    for cur_path in root_path.rglob("*"):
-        if cur_path.is_dir() or not cur_path.suffix == '.url':
-            continue
-        url = cur_path.read_text('utf-8-sig')
-        if not utils.youtube_get_id(url):
-            continue
-        print(cur_path)
-        ydl_opts = {
-            'format': f'best[height<720]+best[ext={settings.video_ext[1:]}],bestaudio[ext={settings.audio_ext[1:]}]',
-            'outtmpl': cur_path.with_suffix('.%(ext)s').as_posix()
-        }
-        with YoutubeDL(ydl_opts) as ydl:
-            ydl.download([url])
+def download(cur_path: Path, url, playlist=False):
+    ydl_opts = dict(format=settings.download_format, ignoreerrors=True)
+
+    if playlist:
+        dir = cur_path.with_suffix('.pls')
+        dir.mkdir(exist_ok=True)
+        ydl_opts['outtmpl'] = dir.as_posix() + '/%(playlist_index)s. %(title)s.%(ext)s'
+    else:
+        ydl_opts['outtmpl'] = cur_path.with_suffix('.%(ext)s').as_posix()
+
+    with YoutubeDL(ydl_opts) as ydl:
+        result = ydl.download([url])
+
+    if result == 0:
         cur_path.rename(cur_path.with_suffix(settings.bak_ext))
 
 
+def main():
+    root_path = Path(settings.root)
+
+    for cur_path in root_path.rglob("*"):
+        if cur_path.is_dir() or not cur_path.suffix == '.url':
+            continue
+
+        url = cur_path.read_text('utf-8-sig')
+        print(cur_path, url)
+
+        if utils.youtube_get_id(url):
+            download(cur_path, url)
+        elif 'youtube.com/playlist' in url:
+            download(cur_path, url, playlist=True)
+
+
 if __name__ == "__main__":
-    download()
+    main()
--- settings.py
+++ settings.py
@@ -6,3 +6,4 @@ bak_ext = '.old'
 root = "/storage/learn/"
 only_files = 'l.electis.ru'
 converting_flag = 'converting.tmp'
+download_format = f'best[height<720]+best[ext={video_ext[1:]}],bestaudio[ext={audio_ext[1:]}]'
